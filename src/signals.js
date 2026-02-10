9b69aaac97ab7c76bf8966ff41a2a12ba4d68927
Bobby Donev <donev.bobby@gmail.com>
2026-02-10 20:29:16 +0200

    feat: add vanilla js reactive effects

--- src/signals.js
+++ src/signals.js
@@ -1,7 +1,16 @@
+// Hold currently running effect so auto tracking works
+// effects update it, so signal getters see who's listening and register subscribers
+let currentEffect = null;
+
+// Create a reactive value
 export function createSignal(value) {
 	const subscribers = new Set();
 
 	const getter = () => {
+		if (currentEffect) {
+			subscribers.add(currentEffect);
+		}
+
 		return value;
 	};
 
@@ -11,4 +20,15 @@ export function createSignal(value) {
 	};
 
 	return [getter, setter];
+};
+
+// Run passed in func every time signal inside it changes
+export function createEffect(fn) {
+	const effect = () => {
+		currentEffect = effect;
+		fn();
+		currentEffect = null;
+	};
+
+	effect();
 };
